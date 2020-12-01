b65dd6f18d0bcbda8c31d2b3b506adbdaf5a1d99
Mayank Kakkar <mayankkakkar1729@gmail.com>
2020-12-02 03:56:46 +0530

    Add Date into the page

--- app.js
+++ app.js
@@ -5,7 +5,16 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
-    res.render('list');
+    let today = new Date();
+    let currentDay = today.getDay();
+    let options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric'
+    }
+    let day = today.toLocaleDateString("en-US", options);
+    res.render('list', { kindOfDay: day });
 })
 
 app.listen(3000, () => console.log("Sever running on Port 3000"));
