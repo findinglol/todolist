9a48140ae67cd91cd28abe44b34d7873059309c5
Mayank Kakkar <mayankkakkar1729@gmail.com>
2020-12-02 04:20:27 +0530

    Add ability to add Items

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const bodyParser = require("body-parser");
 
 const app = express();
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let items = ["Learn EJS", "Learn git"];
 
 app.get("/", (req, res) => {
     let today = new Date();
@@ -14,7 +17,13 @@ app.get("/", (req, res) => {
         year: 'numeric'
     }
     let day = today.toLocaleDateString("en-US", options);
-    res.render('list', { kindOfDay: day });
+    res.render('list', { kindOfDay: day, items: items });
+});
+
+app.post("/", (req, res) => {
+    let item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
 })
 
 app.listen(3000, () => console.log("Sever running on Port 3000"));
