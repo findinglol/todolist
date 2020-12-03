c463d1e56c0740e4ea2594b1371fde652d987bb1
Mayank Kakkar <mayankkakkar1729@gmail.com>
2020-12-04 03:24:33 +0530

    Add work list

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(static('static'));
 let items = ["Learn EJS", "Learn git"];
+let work = [];
 
 app.get("/", (req, res) => {
     let today = new Date();
@@ -18,13 +19,22 @@ app.get("/", (req, res) => {
         year: 'numeric'
     }
     let day = today.toLocaleDateString("en-US", options);
-    res.render('list', { kindOfDay: day, items: items });
+    res.render('list', { pageTitle: day, items: items });
 });
 
 app.post("/", (req, res) => {
     let item = req.body.newItem;
-    items.push(item);
-    res.redirect("/");
+    if (req.body.submitButton === 'Work') {
+        work.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
 })
+
+app.get("/work", (req, res) => {
+    res.render('list', { pageTitle: 'Work', items: work });
+});
 
 app.listen(3000, () => console.log("Sever running on Port 3000"));
